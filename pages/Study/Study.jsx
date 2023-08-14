61f6de1379f2c58ff6f73c0066211e209399c15d
layzduckling <layzduckling@icloud.com>
2023-08-14 21:49:01 +0900

    Add effect when hovering over selection

--- pages/Study/Study.jsx
+++ pages/Study/Study.jsx
@@ -1,32 +1,48 @@
-import React, {useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { StyleSheet, View, SafeAreaView, Text, Animated, PanResponder } from "react-native";
 
 const Study = () => {
     const pan = useRef(new Animated.ValueXY()).current;
 
+    const [oneSelected, setOneSelected] = useState(false);
+    const [twoSelected, setTwoSelected] = useState(false);
+
+    const updateSelection = (y=pan.y._value) => {
+      if (y < -120) {
+        setOneSelected(true);
+      } else if (y > 120) {
+        setTwoSelected(true);
+      } else {
+        setOneSelected(false);
+        setTwoSelected(false);
+      }
+    }
+
     const panResponder = useRef(
       PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}], {useNativeDriver: false}),
+      onPanResponderMove: (e, gestureState) => {
+        updateSelection();
+        Animated.event([null, {dx: pan.x, dy: pan.y}], {useNativeDriver: false})(e, gestureState);
+      },
       onPanResponderRelease: () => {
-        if (pan.y._value < -120) {
-          console.log("Selection 1")
-        }
-        else if (pan.y._value > 120) {
-          console.log("Selection 2")
-        }
         pan.flattenOffset();
         Animated.spring(pan, {
-        toValue: { x: 0, y: 0 },
-        useNativeDriver: false,
+          toValue: { x: 0, y: 0 },
+          useNativeDriver: false,
         }).start();
+        console.log(pan.y._value);
+        updateSelection(0);
       },
       }),
     ).current;
 
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={[styles.memorized, {zIndex: 0}]}>
+        <Text style={[
+            oneSelected ? styles.selected : styles.selection, 
+            {zIndex: 0}
+          ]}>
           Selection 1
         </Text>
         <Animated.View
@@ -40,7 +56,9 @@ const Study = () => {
           {...panResponder.panHandlers}>
           <Text style={styles.word}>Word</Text>
         </Animated.View>
-        <Text style={[styles.notMemorized, {zIndex: 0}]}>
+        <Text style={[
+            twoSelected ? styles.selected : styles.selection, {zIndex: 0}
+          ]}>
           Selection 2
         </Text>
       </SafeAreaView>
@@ -55,7 +73,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   word: {
-    fontSize: 30,
+    fontSize: 32,
     fontWeight: "bold",
     color: "white",
     textAlign: "center",
@@ -63,16 +81,28 @@ const styles = StyleSheet.create({
   box: {
     backgroundColor: "blue",
     borderRadius: 16,
-    padding: 20,
+    padding: 8,
     width: 200
   },
-  memorized: {
-    fontSize: 20,
-    color: "green",
+  selection: {
+    fontSize: 24,
+    textAlign: "center",
+    color: "black",
+    width: 200,
+    padding: 8,
+    borderWidth: 2,
+    borderRadius: 16,
   },
-  notMemorized: {
-    fontSize: 20,
-    color: "red",
+  selected: {
+    fontSize: 24,
+    textAlign: "center",
+    color: "black",
+    backgroundColor: "lightblue",
+    overflow: "hidden",
+    width: 200,
+    padding: 8,
+    borderWidth: 2,
+    borderRadius: 16,
   }
 });
 
