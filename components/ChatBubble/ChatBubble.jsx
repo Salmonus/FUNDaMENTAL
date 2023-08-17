4f7aa6245a8af2402799857a60846dba222ddb8f
layzduckling <layzduckling@icloud.com>
2023-08-17 19:35:54 +0900

    Change UI of chat screen to match the game-style

--- components/ChatBubble/ChatBubble.jsx
+++ components/ChatBubble/ChatBubble.jsx
@@ -4,41 +4,27 @@ import { StyleSheet, View, Text } from "react-native";
 const ChatBubble = ({ text, leftBubble = false }) => {
   return (
     <View style={leftBubble ? styles.chatBubbleLeft : styles.chatBubbleRight}>
-      <Text style={styles.text}>{text}</Text>
+      <Text style={[styles.text, {color: leftBubble ? "lightgrey" : "skyblue"}]}>
+        <Text style={{fontFamily: "ChakraPetch-SemiBold"}}>{leftBubble ? "Learna: " : "You: "}</Text>
+        <Text>{text}</Text>
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   chatBubbleLeft: {
-    alignSelf: "flex-start",
-    borderColor: "#000",
-    backgroundColor: "#000",
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    borderBottomLeftRadius: 2,
-    borderBottomRightRadius: 15,
-    borderWidth: 1,
-    margin: 5,
+    margin: 4,
   },
   chatBubbleRight: {
-    alignSelf: "flex-end",
-    backgroundColor: "#fff",
-    borderColor: "#0601B4",
-    backgroundColor: "#0601B4",
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    borderBottomLeftRadius: 15,
-    borderBottomRightRadius: 2,
-    borderWidth: 1,
-    margin: 5,
+    margin: 4,
   },
   text: {
     color: "#fff",
     fontSize: 16,
-    fontWeight: "regular",
+    fontFamily: "ChakraPetch-Regular",
     textAlign: "left",
-    padding: 10,
+    padding: 8,
   },
 });
 
